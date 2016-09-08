ecb1a37ba419602bd548c75dcb17960202d5485d
Leonard Lausen <leonard@lausen.nl>
2016-09-08 03:03:38 +0000

    emacs: display current time

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -269,6 +269,9 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  ;; Display time in powerline
+  (display-time-mode 1)
+
   ;; Golden ratio mode
   (golden-ratio-mode 1)
 
