e1b95b0cb3680b4abb82205783dc6fbde1002b99
Leonard Lausen <leonard@lausen.nl>
2016-12-01 06:51:41 +0000

    emacs: add octave layer

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -77,6 +77,7 @@ values."
             c-c++-enable-clang-support t
             c-c++-default-mode-for-headers 'c++-mode)
      ess
+     octave
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
