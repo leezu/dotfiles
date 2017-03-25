3e79562338dd0eb3be94d58dc604bc5660ad63ce
Leonard Lausen <leonard@lausen.nl>
2017-03-25 10:39:09 +0000

    emacs: cuda

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -82,7 +82,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(git-auto-commit-mode org-pdfview interleave pydoc)
+   dotspacemacs-additional-packages '(git-auto-commit-mode org-pdfview interleave pydoc cuda-mode)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
@@ -619,6 +619,10 @@ cite:%k
               (setq flycheck-clang-language-standard "c++14")
               (setq flycheck-gcc-language-standard "c++14")
               ))
+
+  ;; Cuda
+  ;; https://github.com/syl20bnr/spacemacs/issues/1815
+  (use-package cuda-mode :defer t)
 
   ;; Python
   (setq python-shell-interpreter "ipython3"
