1f1ff2d09733d49d81c85e053d7b3464e2ba706b
Leonard Lausen <leonard@lausen.nl>
2017-04-10 12:24:56 +0000

    fish: add fish shell configuration

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,30 @@
+## Aliases
+# Dictionaries
+alias ddevil='dict -d devil'
+alias wn='dict -d wn'
+alias ee='dict -d moby-thesaurus'
+alias ed='dict -d fd-eng-deu'
+alias de='dict -d fd-deu-eng'
+# Info pages and manuals
+alias info='info --vi-keys'
+
+# Local software
+set -x PATH $HOME/.local/bin $PATH
+set -x CPATH $HOME/.local/include $CPATH # cpp searches for include files here (like -I )
+set -x LIBRARY_PATH $HOME/.local/lib $LIBRARY_PATH # link time libraries
+set -x LD_LIBRARY_PATH $HOME/.local/lib $LD_LIBRARY_PATH # run time libraries
+
+# NPM
+set -x NPM_PACKAGES $HOME/develop/npm-packages
+set -x PATH $NPM_PACKAGES/bin $PATH
+set -e MANPATH
+set -x MANPATH $NPM_PACKAGES/share/man $(manpath)
+set -x NODE_PATH $NPM_PACKAGES/lib/node_modules $NODE_PATH
+
+# Ruby gems
+set -e PATH /home/leonard/.gem/ruby/2.3.0/bin $PATH
+
+# Preferred editor for local and remote sessions
+# TODO implement this once using fish shell on dycpu: set default to vim then
+set -e EDITOR "emacsclient -c --tty --alternate-editor=''"
+alias emacs="emacsclient -c --tty --alternate-editor=''"
