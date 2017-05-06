8418cc785db777f6a5e94c8f4bb9d25bf345d02e
Leonard Lausen <leonard@lausen.nl>
2017-05-06 16:01:15 +0000

    shell: alias to toggle capslock

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -7,6 +7,8 @@ alias ed='dict -d fd-eng-deu'
 alias de='dict -d fd-deu-eng'
 # Info pages and manuals
 alias info='info --vi-keys'
+# Toggle capslock
+alias capslock_toggle="python -c 'from ctypes import *; X11 = cdll.LoadLibrary("libX11.so.6"); display = X11.XOpenDisplay(None); X11.XkbLockModifiers(display, c_uint(0x0100), c_uint(2), c_uint(0)); X11.XCloseDisplay(display)'"
 
 ## Variables
 ## fish automatically creates arrays from the variables PATH, CDPATH and MANPATH when it is started
