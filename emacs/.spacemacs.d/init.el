b163d0f01e6ac48be8a040f979d5b593833b1615
Leonard Lausen <leonard@lausen.nl>
2017-10-09 08:54:08 +0000

    emacs: enable persistent-scratch

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -100,7 +100,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(git-auto-commit-mode pydoc cuda-mode)
+   dotspacemacs-additional-packages '(git-auto-commit-mode pydoc cuda-mode persistent-scratch)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
@@ -375,6 +375,9 @@ you should place your code here."
 
   ;; Multi-cursor
   (global-evil-mc-mode  1)
+
+  ;; Persistent scratch buffer
+  (persistent-scratch-setup-default)
 
   ;;
   ;; Layouts and workspaces
