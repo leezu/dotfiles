1f96aee161a6d7946b8fc47ee75e54a6419babc4
Leonard Lausen <leonard@lausen.nl>
2017-10-21 13:42:30 +0000

    emacs: location based agenda auto-exclude-function

--- emacs/.spacemacs.d/layers/my-org/funcs.el
+++ emacs/.spacemacs.d/layers/my-org/funcs.el
@@ -11,12 +11,14 @@
 
 
 (defun leezu/weekday-p ()
+  "Return t if today is a weekday."
   (let ((wday (nth 6
                    (decode-time))))
     (and (< wday 6)
          (> wday 0))))
 
 (defun leezu/working-p ()
+  "Return t on weekdays during office hours."
   (let ((hour (nth 2
                    (decode-time))))
     (and (leezu/weekday-p)
@@ -25,8 +27,22 @@
              (and (>= hour 12)
                   (<= hour 19))))))
 
+(defun leezu-location-cdr ()
+  (let (result)
+    (dolist (location-list my-org-location-lists result)
+      (setq result (cl-member my-org-location location-list :test 'string=)))
+    result))
+
+(defun leezu/location-available (location)
+  "Return t if location is compatible, i.e. a superset of the current location."
+  (cl-member location
+             (leezu-location-cdr)
+             :test 'string=))
+
 (defun leezu/org-auto-exclude-function (tag)
   (and (cond
+        ((cl-member tag my-org-locations :test 'string=) ; location tag
+         (not (leezu/location-available tag)))
         ((string= tag "@home")
          (leezu/working-p))
         ((string= tag "@office")
