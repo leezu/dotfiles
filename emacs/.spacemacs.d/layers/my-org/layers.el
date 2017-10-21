c50701b0850d4e37c8c8590f8ab83e9b2a8b2958
Leonard Lausen <leonard@lausen.nl>
2017-10-21 10:04:09 +0000

    emacs: correctly declare dependency on org and bibtex layer in my-org

---
+++ emacs/.spacemacs.d/layers/my-org/layers.el
@@ -0,0 +1,13 @@
+;;; layers.el --- my-org layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2017 Leonard Lausen
+;;
+;; Author:  <leonard@lausen.nl>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(configuration-layer/declare-layer 'org)
+(configuration-layer/declare-layer 'bibtex)
