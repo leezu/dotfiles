25880357c5744233cf536e499361523e5db3ed9b
Leonard Lausen <leonard@lausen.nl>
2017-10-26 17:28:26 +0000

    emacs: enable nixos layer

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -94,6 +94,7 @@ This function should only modify configuration layer settings."
             c-c++-default-mode-for-headers 'c++-mode)
      octave
      shell-scripts
+     nixos
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
