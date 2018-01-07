8c56f992d0a3c208138c432fa1ac1868ece043b3
Leonard Lausen <leonard@lausen.nl>
2018-01-07 13:28:12 +0000

    emacs: Enable high contrast manoj-dark theme
    Good visibility with minimal screen backlight. To save power on battery.

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -185,8 +185,9 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(spacemacs-dark
-                         leuven
-                         spacemacs-light)
+               manoj-dark
+               leuven
+               spacemacs-light)
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    ;; (default t)
    dotspacemacs-colorize-cursor-according-to-state t
