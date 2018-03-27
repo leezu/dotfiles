756bac9f6d45f685b9ad4c56e60ebd2ee524f2e5
Leonard Lausen <leonard@lausen.nl>
2018-03-27 02:38:06 +0000

    emacs: append system-configuration to elpa subdirectory

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -158,7 +158,7 @@ It should only modify the values of Spacemacs settings."
    ;; If non-nil, a form that evaluates to a package directory. For example, to
    ;; use different package directories for different Emacs versions, set this
    ;; to `emacs-version'. (default 'emacs-version)
-   dotspacemacs-elpa-subdirectory 'emacs-version
+   dotspacemacs-elpa-subdirectory '(concat emacs-version system-configuration)
    ;; One of `vim', `emacs' or `hybrid'.
    ;; `hybrid' is like `vim' except that `insert state' is replaced by the
    ;; `hybrid state' with `emacs' key bindings. The value can also be a list
