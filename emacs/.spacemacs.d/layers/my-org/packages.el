ca118c8fe44aff22f92c7cf90cf7ee752388f460
Leonard Lausen <leonard@lausen.nl>
2018-10-01 11:17:20 +0000

    emacs: enable org-agenda-sticky

--- emacs/.spacemacs.d/layers/my-org/packages.el
+++ emacs/.spacemacs.d/layers/my-org/packages.el
@@ -135,6 +135,7 @@
 ;;;; org-agenda
 (defun my-org/post-init-org-agenda ()
   (setq org-agenda-skip-scheduled-if-deadline-is-shown t
+        org-agenda-sticky t
 
         org-agenda-files `("~/org/organizer.org"
                            "~/org/areas/"
