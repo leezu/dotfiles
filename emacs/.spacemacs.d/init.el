7720c6e811267fe1894d34afe5e93a88d9c29358
Leonard Lausen <leonard@lausen.nl>
2019-03-29 05:32:53 +0000

    emacs: elfeed

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -73,6 +73,9 @@ This function should only modify configuration layer settings."
               notmuch-kill-tags '("+killed" "-inbox" "-autoinbox"))
      slack
 
+     ;; News
+     elfeed
+
      ;; Finance
      finance
 
@@ -139,7 +142,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(pydoc cuda-mode cheat-sh graphql-mode flycheck-cython gscholar-bibtex)
+   dotspacemacs-additional-packages '(pydoc cuda-mode cheat-sh graphql-mode flycheck-cython gscholar-bibtex	elfeed-protocol)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
