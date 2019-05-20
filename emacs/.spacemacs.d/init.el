c20e8ce390ecb74063bba22a25bc7e6f077a7cb7
Leonard Lausen <leonard@lausen.nl>
2019-05-20 19:12:43 +0000

    emacs: update dotspacemacs-additional-packages
    remove elfeed
    add academic-phrases and jupyter

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -143,7 +143,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(pydoc cuda-mode cheat-sh graphql-mode flycheck-cython gscholar-bibtex	elfeed-protocol)
+   dotspacemacs-additional-packages '(pydoc cuda-mode cheat-sh graphql-mode flycheck-cython gscholar-bibtex academic-phrases jupyter)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
