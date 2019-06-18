56a356d43fce332f6a7fba0751ee8499c7156e64
Leonard Lausen <lausen@amazon.com>
2019-06-18 03:13:39 +0000

    shell: improve syncthing conflict detection
    Don't warn for backups of removed conflicts held in the .stversions folder.

--- shell/.shellrc/rc.d/warn.sh
+++ shell/.shellrc/rc.d/warn.sh
@@ -8,7 +8,7 @@ fi
 
 # Warn for syncthing conflicts in ~/org
 if [ -d "$HOME/org" ]; then
-    conflictcount=$(find $HOME/org -name "*.sync-conflict-*" | wc -l)
+    conflictcount=$(find $HOME/org -name "*.sync-conflict-*" -not -path "*.stversions*" | wc -l)
     if [ $conflictcount -gt 0 ] ; then
         echo -e "\e[31m$conflictcount *.sync-conflict-* files in ~/org"
     fi
@@ -16,7 +16,7 @@ fi
 
 # Warn for syncthing conflicts in ~/wiki
 if [ -d "$HOME/wiki" ]; then
-    conflictcount=$(find $HOME/wiki -name "*.sync-conflict-*" | wc -l)
+    conflictcount=$(find $HOME/wiki -name "*.sync-conflict-*" -not -path "*.stversions*" | wc -l)
     if [ $conflictcount -gt 0 ] ; then
         echo -e "\e[31m$conflictcount *.sync-conflict-* files in ~/wiki"
     fi
