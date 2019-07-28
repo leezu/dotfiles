2844a84a7571fffd0afcfb2330e746053657b574
Leonard Lausen <leonard@lausen.nl>
2019-07-28 18:50:13 +0000

    emacs: add \usepackage{mathtools} to org-format-latex-header

--- emacs/.spacemacs.d/layers/my-org/packages.el
+++ emacs/.spacemacs.d/layers/my-org/packages.el
@@ -46,6 +46,27 @@
         org-agenda-skip-scheduled-if-done t  ;; Don't show DONE tasks in time-grid
         org-preview-latex-default-process 'dvisvgm
         org-format-latex-options (plist-put org-format-latex-options :scale 2.0))
+  ;; Latex
+  (setq org-format-latex-header "\\documentclass{article}
+\\usepackage[usenames]{color}
+% add mathtools here, as org-latex-packages-alist is not used for inline latex fragments
+\\usepackage{mathtools}
+[PACKAGES]
+[DEFAULT-PACKAGES]
+\\pagestyle{empty}             % do not remove
+% The settings below are copied from fullpage.sty
+\\setlength{\\textwidth}{\\paperwidth}
+\\addtolength{\\textwidth}{-3cm}
+\\setlength{\\oddsidemargin}{1.5cm}
+\\addtolength{\\oddsidemargin}{-2.54cm}
+\\setlength{\\evensidemargin}{\\oddsidemargin}
+\\setlength{\\textheight}{\\paperheight}
+\\addtolength{\\textheight}{-\\headheight}
+\\addtolength{\\textheight}{-\\headsep}
+\\addtolength{\\textheight}{-\\footskip}
+\\addtolength{\\textheight}{-3cm}
+\\setlength{\\topmargin}{1.5cm}
+\\addtolength{\\topmargin}{-2.54cm}")
 
 ;;;;; org-id
   (require 'org-id)
