a48334c3edae707066cc995e6f5b2797fd8552ea
Leonard Lausen <lausen@amazon.com>
2019-08-19 09:01:06 +0000

    shell: PKG_CONFIG_PATH, GSETTINGS_SCHEMA_DIR and XDG_DATA_DIRS
    Required for https://wiki.gnome.org/Apps/Evolution/Building and libical

--- shell/.shellrc/env.d/localpaths.sh
+++ shell/.shellrc/env.d/localpaths.sh
@@ -4,3 +4,6 @@ export PATH="$HOME/local/bin${PATH:+:${PATH}}"
 export CPATH="$HOME/local/include${CPATH:+:${CPATH}}" # cpp searches for include files here (like -I )
 export LIBRARY_PATH="$HOME/local/lib${LIBRARY_PATH:+:${LIBRARY_PATH}}" # link time libraries
 export LD_LIBRARY_PATH="$HOME/local/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" # run time libraries
+export PKG_CONFIG_PATH="$HOME/local/lib/pkgconfig:$HOME/local/share/pkgconfig" # make sure pkgconfig can find local packages
+export GSETTINGS_SCHEMA_DIR="$HOME/local/share/glib-2.0/schemas"
+export XDG_DATA_DIRS="$HOME/local/share${XDG_DATA_DIRS:+:${XDG_DATA_DIRS}}"
