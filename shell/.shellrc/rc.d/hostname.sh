af13bc201669888de0f8a978c579130613b149c3
Leonard Lausen <leonard@lausen.nl>
2026-03-18 14:08:14 +0000

    shell: update defaults for ec2 instances

--- shell/.shellrc/rc.d/hostname.sh
+++ shell/.shellrc/rc.d/hostname.sh
@@ -2,6 +2,6 @@ export HOST=`hostname`
 if [[ -f ~/.sharedshellrc-$HOST ]]; then
     source ~/.sharedshellrc-$HOST
 fi
-if [[ "$OSTYPE" == linux-gnu ]] && curl -s --max-time 0.01 http://169.254.169.254/latest/dynamic/instance-identity/document | grep -q availabilityZone; then
+if [[ "$OSTYPE" == linux-gnu ]] && curl -sf --max-time 0.01 -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 1" -o /dev/null 2>/dev/null; then
     source ~/.sharedshellrc-aws
 fi
